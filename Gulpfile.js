88f35b291ab567e48e80bbdd4ee5b2d81ee4f4eb
Javier Aroche <aroche290@gmail.com>
2017-06-18 17:43:56 -0700

    update(): code celanup and added gulp

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+	gulp.src('styles/iom.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('styles'));
+});
+
+//Watch task
+gulp.task('default',function() {
+	gulp.watch('styles/**/*.scss',['styles']);
+});
